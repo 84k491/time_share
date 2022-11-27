ca42226626421d6d9c1070732f30283dcb4e3bb9
84k491 <you@example.com>
2022-11-27 12:16:40 +0300

    Avoid clock fluctuations

--- common/timer.h
+++ common/timer.h
@@ -5,10 +5,10 @@
 class Timer
 {
 public:
-    int work(std::function<int()> callback, std::chrono::microseconds interval, size_t limit = 0)
+    int work(std::function<int()> callback, std::chrono::milliseconds interval, size_t limit = 0)
     {
         int rc = 0;
-        auto timepoint = std::chrono::steady_clock::now();
+        auto timepoint = std::chrono::ceil<std::chrono::milliseconds>(std::chrono::steady_clock::now());
         size_t count = 0;
         while ((!m_need_to_stop.load() || 0 == rc) &&
             ((0 == limit) != limit > count)) {
